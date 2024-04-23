0931a34d7dfc3dd9d8abfc07ab017da0b6518550
Mohammed Sajidul Islam <sajidislam729@gmail.com>
2024-04-23 18:47:07 +0600

    project init

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,10 +1,13 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useRef } from 'react'
+import { AiOutlineMenuFold } from 'react-icons/ai'
 import { NavLink, useLocation } from 'react-router-dom'
 
 export default function Nav() {
+    const menu_icon_ref = useRef()
+    const cross_icon_ref = useRef()
+    const nav = useRef()
 
-
-    const MenuItem = ({ icon, name, to }) => {
+    const MenuItem = ({ name, to }) => {
         const { pathname } = useLocation()
         return (
             <>
@@ -15,26 +18,24 @@ export default function Nav() {
         )
     }
 
+    function close_menu() {
+        nav?.current.classList.remove("active");
+        document.body.classList.remove("menu-active")
+    }
 
-    // useEffect(() => {
-    //     let menu_icon = document.getElementById('menu_icon')
-    //     let cross_icon = document.getElementById('cross_icon')
-    //     let nav = document.getElementById('sidebar')
-    //     menu_icon.addEventListener('click', () => {
-    //         nav.classList.add("active");
-    //         document.body.classList.add("menu-active")
-    //     })
-    //     function close_menu() {
-    //         nav.classList.remove("active");
-    //         document.body.classList.remove("menu-active")
-    //     }
-    //     document.body.addEventListener('click', (e) => {
-    //         if (nav.classList.contains('active') && e.target != menu_icon && (e.target != nav || e.target == cross_icon)) {
-    //             console.log(true)
-    //             close_menu()
-    //         }
-    //     })
-    // })
+    useEffect(() => {
+        function handleClickOutside(event) {
+            if (!nav?.current.contains(event.target) && !menu_icon_ref?.current.contains(event.target)) {
+                close_menu();
+            }
+        }
+
+        document.addEventListener('click', handleClickOutside);
+
+        return () => {
+            document.removeEventListener('click', handleClickOutside);
+        };
+    }, []);
 
     return (
         <header className="">
@@ -42,8 +43,8 @@ export default function Nav() {
                 <a href="index.html" className="text-4xl font-semibold">
                     <img className="w-[120px]" src="./images/logo.png" alt="" />
                 </a>
-                <nav id="sidebar" className="flex items-center gap-4">
-                    <div id="cross_icon" className="md:hidde absolute top-[20px] right-[20px] cursor-pointer text-2xl">
+                <nav ref={nav} id="sidebar" className="flex items-center gap-4">
+                    <div ref={cross_icon_ref} onClick={close_menu} id="cross_icon" className="md:hidde absolute top-[40px] right-[30px] cursor-pointer text-2xl">
                         ✕
                     </div>
                     <MenuItem name="Home" to='/' />
@@ -52,12 +53,17 @@ export default function Nav() {
                     <MenuItem name="Project" to='/project' />
                     <MenuItem name="Contact" to='/contact' />
                 </nav>
-                <div id="menu_icon" className="md:hidden flex gap-1 items-center flex-col justify-center cursor-pointer">
-                    <span className="w-[20px] h-[2px] bg-black"></span>
-                    <span className="w-[20px] h-[2px] bg-black"></span>
-                    <span className="w-[20px] h-[2px] bg-black"></span>
+                <div
+                    ref={menu_icon_ref}
+                    className="md:hidden menu_icon flex gap-1 items-center flex-col justify-center cursor-pointer"
+                    onClick={() => {
+                        nav?.current.classList.add("active");
+                        document.body.classList.add("menu-active")
+                    }}
+                >
+                    <AiOutlineMenuFold className='text-2xl text-black' />
                 </div>
             </div>
-        </header>
+        </header >
     )
 }
