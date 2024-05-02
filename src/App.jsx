9b697d5b7668a86fe144f031659c88a3855cfe20
Mohammed Sajidul Islam <sajidislam729@gmail.com>
2024-05-02 20:24:58 +0600

    seo and title added

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,26 @@ import Project from "./pages/Project";
 import Contact from "./pages/Contact";
 import PageNotFound from "./pages/PageNotFound";
 import Gallery from "./pages/Gallery";
+import { Helmet } from "react-helmet";
+
+/*
+  - Title
+  - Description
+  - Keywords
+  - Alt tag on image
+  - Heading tags on page
+  - Canonical tags
+  - Opengraph information
+*/
 
 
 function App() {
   return (
     <div className="App">
+      {/* <Helmet>
+        <title>Shiraji General Contracting</title>
+        <meta name="description" content="Helmet application" />
+      </Helmet> */}
       <Nav />
       <Routes>
         <Route path='/' element={<Home />} />
